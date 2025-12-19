8a9546fd873fb425719031608c31beaecdc4cce7
FadyKhacheba21313 <fadysenpai@gmail.com>
2025-12-20 00:37:37 +0100

    Add files via upload

---
+++ athleteee.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {	
+		typedef struct{
+		int heure,min,sec}
+		Time;
+	typedef struct{
+
+	int numero;
+	char name[25];
+	time resultat;
+	} athlete;
+	
+		int transforme(time t){
+		return(t.heure*3600+t.min*60+t.sec);
+	
+	int N;
+	do{scanf("%d",&N);}while (N<=0 ||N>=100)
+	}
+	athlete T[N];
+	int i; 
+	int sum=0;
+	for (i=0;i<N;i++)
+	printf("give the numero of athlete");
+	scanf("%d",&T[i].nuemro);
+		printf("give the name of athlete");
+	scanf("%c",&T[i].name);
+		printf("give the resultat of athlete");
+	scanf("%d",&T[i].resultat.time.heure);
+	scanf("%d",&T[i].resultat.time.min);
+	scanf("%d",&T[i].resultat.time.sec);
+
+sum=sum+transforme(T[i],result);
+	
+	}
+	double moy=sum/N;
+		for (i=0;i<N;i++)
+if ( trasforme(T[i].resultat) <= moy){
+	printf("%s",T[i].name);
+		printf("%s",T[i].name);
+	printf("%s",T[i].name);
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
